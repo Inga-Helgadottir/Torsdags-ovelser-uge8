177aef694e12c787bb66baeb303cc5a1adfcfb86
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-02-24 15:06:23 +0100

    Create Main.java

---
+++ Task1/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+
+	public static void main(String[] args){ 
+		printString("haha");
+		emptyLine();
+		printSentence("Inga", 25);
+	}
+
+	public static void printString(String myString){
+		System.out.println(myString);
+	}
+
+	public static void emptyLine(){
+		System.out.println();
+	}
+
+	public static void printSentence(String name, int age){
+		System.out.println("My name is " + name + ", I am " + age + " years old");
+	}
+}
