e4bd66bce22ad2a37fc771e7a3ca903f60fc4cb6
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-02-25 11:47:05 +0100

    Create Student.java

---
+++ Task5/Student.java
@@ -0,0 +1,14 @@
+public class Student{
+	String name;
+	int age;
+	boolean isFemale;
+	boolean datamatikerTeam;//--------------------------------------------
+
+	public Student(String tmpName, int tmpAge, boolean tmpIsFemale, boolean tmpDatamatikerTeam){
+		this.name = tmpName;
+		this.age = tmpAge;
+		this.isFemale = tmpIsFemale;
+		this.datamatikerTeam = tmpDatamatikerTeam;
+	}
+
+}
